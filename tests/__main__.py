5c4792b487cd910b84abaf8c6eab46ae16481947
MhdHO98 <167417592+MhdHO98@users.noreply.github.com>
2024-05-06 18:45:52 +0400

    Delete tests directory

--- tests/__main__.py
+++
@@ -1,7 +0,0 @@
-from .test import play
-
-if __name__ == "__main__":
-    
-  players = [HMAIPlayer("AI"), HMHumanPlayer("Moha")]
-  game = HMGame(players)
-  game.play()
--- tests/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from Classes import HMPlayer, HMHumanPlayer, HMAIPlayer
-from game import HMGame
-
-players = [HMAIPlayer("AI"), HMHumanPlayer("Moha")]
-game = HMGame(players)
-game.play()
