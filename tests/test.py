0c6ac8661bc7a992654820a3b4014a6443079323
MhdHO98 <167417592+MhdHO98@users.noreply.github.com>
2024-05-06 18:11:50 +0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from Hangman.Classes import HMPlayer, HMHumanPlayer, HMAIPlayer, HMGame
+from Hangman.Classes import HMPlayer, HMHumanPlayer, HMAIPlayer
+from game import HMGame
 
 players = [HMAIPlayer("AI"), HMHumanPlayer("Moha")]
 game = HMGame(players)
