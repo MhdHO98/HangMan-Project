ccf7183dc7bd57e0578ecd053b342305895ae0a4
MhdHO98 <167417592+MhdHO98@users.noreply.github.com>
2024-05-07 01:35:40 +0400

    Update __main__.py

--- Hangman/__main__.py
+++ Hangman/__main__.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
 
+from Classes import HMPlayer, HMHumanPlayer, HMAIPlayer
+from game import HMGame
+
+def main():
+  
+    players = [HMAIPlayer("AI"), HMHumanPlayer("Moha")]
+    game = HMGame(players)
+    game.play()
+
+if __name__ == '__main__':
+    main()
